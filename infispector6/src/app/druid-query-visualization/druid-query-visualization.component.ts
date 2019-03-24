a54b49320a0b9252bf99b41cb225d7ee507e5258
tveskrna <tveskrna@redhat.com>
2019-03-24 11:03:48 +0100

    Add Time-line to visualization page

--- infispector6/src/app/druid-query-visualization/druid-query-visualization.component.ts
+++ infispector6/src/app/druid-query-visualization/druid-query-visualization.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import {DruidLibraryService} from '../shared/tools/druid-library/druid-library.service';
 
 @Component({
   selector: 'druid-query-visualization',
@@ -7,6 +8,6 @@ import {Component} from '@angular/core';
 })
 
 export class DruidQueryVisualizationComponent {
-  constructor() {}
+  constructor(private druidLibrary:DruidLibraryService) {}
 }
 
--- infispector6/src/app/druid-query-visualization/druid-query-visualization.module.ts
+++ infispector6/src/app/druid-query-visualization/druid-query-visualization.module.ts
@@ -2,18 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { DruidQueryVisualizationComponent } from './druid-query-visualization.component';
+import {TimeLineModule} from '../shared/graphs/time-line/time-line.module';
+// import {MessageFlowModule} from '../shared/graphs/message-flow/message-flow.module';
 
 @NgModule({
   declarations: [
     DruidQueryVisualizationComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    TimeLineModule,
+    // MessageFlowModule
   ],
   exports: [
     DruidQueryVisualizationComponent
   ],
   providers: [],
-  bootstrap: [DruidQueryVisualizationComponent]
+  // bootstrap: [DruidQueryVisualizationComponent]
 })
 export class DruidQueryVisualizationModule { }
